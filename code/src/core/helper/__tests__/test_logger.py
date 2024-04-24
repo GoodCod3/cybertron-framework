10aff453d8be99c1cc49dfc6c91dac92effcd8a7
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-04-24 12:55:26 +0200

    Fix lint

--- code/src/core/helper/__tests__/test_logger.py
+++ code/src/core/helper/__tests__/test_logger.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest.mock import patch
-from freezegun import freeze_time
 
 from core.helper.logger import Logger
+from freezegun import freeze_time
 
 
 class TestLogger(unittest.TestCase):
@@ -12,30 +12,38 @@ class TestLogger(unittest.TestCase):
     @freeze_time("2022-1-1")
     def test_info_logs_correctly(self):
         expected_result = "[01/01/2022 00:00:00] INFO | Test message"
-        
+
         self.assertEqual(self.logger.info("Test message"), expected_result)
 
     @freeze_time("2022-1-1 12:00:00")
-    @patch('traceback.format_exc')
+    @patch("traceback.format_exc")
     def test_warning_logs_correctly(self, mock_format_exc):
         mock_format_exc.return_value = "Traceback Error test"
-        expected_result = "[01/01/2022 12:00:00] WARNING | Test message | Traceback Error test"
-        
-        self.assertEqual(self.logger.warning("Test message", from_exception=True), expected_result)
+        expected_result = "[01/01/2022 12:00:00] WARNING | Test message | Traceback Error test"  # noqa: E501
+
+        self.assertEqual(
+            self.logger.warning("Test message", from_exception=True),
+            expected_result,
+        )
 
     @freeze_time("2022-1-1 12:00:00")
-    @patch('traceback.format_exc')
+    @patch("traceback.format_exc")
     def test_error_logs_correctly(self, mock_format_exc):
         mock_format_exc.return_value = "Traceback Error test"
-        expected_result = "[01/01/2022 12:00:00] ERROR | Test message | Traceback Error test"
+        expected_result = "[01/01/2022 12:00:00] ERROR | Test message | Traceback Error test"  # noqa: E501
 
-        self.assertEqual(self.logger.error("Test message", from_exception=True), expected_result)
-    
+        self.assertEqual(
+            self.logger.error("Test message", from_exception=True),
+            expected_result,
+        )
+
     @freeze_time("2022-1-1 12:00:00")
-    @patch('traceback.format_exc')
+    @patch("traceback.format_exc")
     def test_critical_logs_correctly(self, mock_format_exc):
         mock_format_exc.return_value = "Traceback Error test"
-        expected_result = "[01/01/2022 12:00:00] CRITICAL | Test message | Traceback Error test"
+        expected_result = "[01/01/2022 12:00:00] CRITICAL | Test message | Traceback Error test"  # noqa: E501
 
-        self.assertEqual(self.logger.critical("Test message", from_exception=True), expected_result)
-
+        self.assertEqual(
+            self.logger.critical("Test message", from_exception=True),
+            expected_result,
+        )
