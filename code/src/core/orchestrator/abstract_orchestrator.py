177181ff85f887ee925fa9a5d279041c07a06835
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-04-24 13:08:47 +0200

    Fix lint

--- code/src/core/orchestrator/abstract_orchestrator.py
+++ code/src/core/orchestrator/abstract_orchestrator.py
@@ -1,4 +1,5 @@
 from code.test_setup import *  # noqa: F401, F403
+
 from src.core.helper.benchmark import Benchmark
 from src.core.helper.logger import Logger
 from src.core.input.input_manager_interface import IInputManager
