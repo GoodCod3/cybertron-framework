0d1d66cf16ca7a94b390e176e5518a750a938e50
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-05-03 13:25:16 +0200

    Fixing lint

--- cybertron_framework/helper/__tests__/test_logger.py
+++ cybertron_framework/helper/__tests__/test_logger.py
@@ -14,7 +14,7 @@ class TestLogger(unittest.TestCase):
     @patch("cybertron_framework.helper.logger.logging")
     def test_info_logs_correctly(self, logging_mock):
         self.logger.info("Test message")
-        
+
         logging_mock.info.assert_called_with("Test message")
 
     @patch("cybertron_framework.helper.logger.logging")
@@ -23,31 +23,26 @@ class TestLogger(unittest.TestCase):
         mock_format_exc.return_value = "Traceback Error test"
         expected_result = "Test message | Traceback Error test"  # noqa: E501
 
-        
         self.logger.warning("Test message", from_exception=True)
-            
+
         logging_mock.warning.assert_called_with(expected_result)
-        
+
     @patch("cybertron_framework.helper.logger.logging")
     @patch("traceback.format_exc")
     def test_error_logs_correctly(self, mock_format_exc, logging_mock):
         mock_format_exc.return_value = "Traceback Error test"
         expected_result = "Test message | Traceback Error test"  # noqa: E501
 
-        
         self.logger.error("Test message", from_exception=True)
-            
+
         logging_mock.error.assert_called_with(expected_result)
 
-        
     @patch("cybertron_framework.helper.logger.logging")
     @patch("traceback.format_exc")
     def test_critical_logs_correctly(self, mock_format_exc, logging_mock):
         mock_format_exc.return_value = "Traceback Error test"
         expected_result = "Test message | Traceback Error test"  # noqa: E501
 
-        
         self.logger.critical("Test message", from_exception=True)
-            
-        logging_mock.critical.assert_called_with(expected_result)
 
+        logging_mock.critical.assert_called_with(expected_result)
--- cybertron_framework/helper/logger.py
+++ cybertron_framework/helper/logger.py
@@ -1,6 +1,5 @@
-import traceback
 import logging
-
+import traceback
 
 logging.basicConfig(
     level=logging.INFO,
@@ -27,13 +26,19 @@ class Logger:
         return logging.info(message)
 
     def warning(self, message, from_exception=False):
-        return logging.warning(self.__build_error_message(message, from_exception))
+        return logging.warning(
+            self.__build_error_message(message, from_exception)
+        )
 
     def error(self, message, from_exception=False):
-        return logging.error(self.__build_error_message(message, from_exception))
+        return logging.error(
+            self.__build_error_message(message, from_exception)
+        )
 
     def critical(self, message, from_exception=False):
-        return logging.critical(self.__build_error_message(message, from_exception))
+        return logging.critical(
+            self.__build_error_message(message, from_exception)
+        )
 
     def __build_error_message(self, message, from_exception=False):
         if from_exception:
